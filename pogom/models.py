df8af0e06e9acd6bae5a71cb2bf77c31a7105372
Frederic Lapatschek <favll@users.noreply.github.com>
2016-07-20 00:26:40 +0200

    Added json response parsing

--- pogom/models.py
+++ pogom/models.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from peewee import *  # Change this
+from peewee import Model, SqliteDatabase, InsertQuery, IntegerField,\
+                   CharField, FloatField, BooleanField, DateTimeField
+from datetime import datetime
+from base64 import b64encode
 
 db = SqliteDatabase('pogom.db')
 
@@ -12,18 +15,21 @@ class BaseModel(Model):
 
 
 class Pokemon(BaseModel):
-    encounter_id = IntegerField(primary_key=True)
+    encounter_id = CharField(primary_key=True)
     spawnpoint_id = CharField()
     pokemon_id = IntegerField()
-    latitude = FloatField()
     longitude = FloatField()
+    latitude = FloatField()
     disappear_time = DateTimeField()
 
 
 class Pokestop(BaseModel):
-    pokestop_id = CharField(unique=True)
+    pokestop_id = CharField(primary_key=True)
+    enabled = BooleanField()
     latitude = FloatField()
     longitude = FloatField()
+    last_modified = DateTimeField()
+    lure_expiration = DateTimeField(null=True)
 
 
 class Gym(BaseModel):
@@ -32,11 +38,71 @@ class Gym(BaseModel):
     TEAM_VALOR = 2
     TEAM_INSTINCT = 3
 
-    gym_id = CharField(unique=True)
+    gym_id = CharField(primary_key=True)
     team_id = IntegerField()
-    team_name = CharField()
-    lat = FloatField()
-    lon = FloatField()
+    guard_pokemon_id = IntegerField()
+    enabled = BooleanField()
+    latitude = FloatField()
+    longitude = FloatField()
+    last_modified = DateTimeField()
+
+
+def parse_dict(d):
+    pokemons = {}
+    pokestops = {}
+    gyms = {}
+
+    cells = d['responses']['GET_MAP_OBJECTS']['map_cells']
+    for cell in cells:
+        for p in cell.get('wild_pokemons', []):
+            pokemons[p['encounter_id']] = {
+                'encounter_id': b64encode(str(p['encounter_id'])),
+                'spawnpoint_id': p['spawnpoint_id'],
+                'pokemon_id': p['pokemon_data']['pokemon_id'],
+                'latitude': p['latitude'],
+                'longitude': p['longitude'],
+                'disappear_time': datetime.fromtimestamp(
+                    (p['last_modified_timestamp_ms'] +
+                     p['time_till_hidden_ms']) / 1000.0)
+            }
+
+        for f in cell.get('forts', []):
+            if f.get('type') == 1:  # Pokestops
+                if 'lure_info' in f:
+                    lure_expiration = datetime.fromtimestamp(
+                        f['lure_info']['lure_expires_timestamp_ms'] / 1000.0)
+                else:
+                    lure_expiration = None
+
+                pokestops[f['id']] = {
+                    'pokestop_id': f['id'],
+                    'enabled': f['enabled'],
+                    'latitude': f['latitude'],
+                    'longitude': f['longitude'],
+                    'last_modified': datetime.fromtimestamp(
+                        f['last_modified_timestamp_ms'] / 1000.0),
+                    'lure_expiration': lure_expiration
+                }
+
+            else:  # Currently, there are only stops and gyms
+                gyms[f['id']] = {
+                    'gym_id': f['id'],
+                    'team_id': f['owned_by_team'],
+                    'guard_pokemon_id': f['guard_pokemon_id'],
+                    'enabled': f['enabled'],
+                    'latitude': f['latitude'],
+                    'longitude': f['longitude'],
+                    'last_modified': datetime.fromtimestamp(
+                        f['last_modified_timestamp_ms'] / 1000.0),
+                }
+
+    print pokemons.values()
+    db.connect()
+    iq = InsertQuery(Pokemon, rows=pokemons.values())
+    iq.upsert()
+    iq.execute()
+    # InsertQuery(Pokestop, rows=pokestops.values()).upsert().execute()
+    # InsertQuery(Gym, rows=gyms.values()).upsert().execute()
 
 
 def create_tables():
