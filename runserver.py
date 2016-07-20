9575f087ff46e9da91cea6d83782053436b8a03d
nbo <n.borrmann@googlemail.com>
2016-07-20 11:34:20 +0200

    made background thread loop, persisted login

--- runserver.py
+++ runserver.py
@@ -7,13 +7,16 @@ from threading import Thread
 
 from pogom import config
 from pogom.app import Pogom
-from pogom.search import search
+from pogom.search import search_loop
 from pogom.utils import get_args, insert_mock_data
 from pogom.models import create_tables
+from pogom.pgoapi.utilities import get_pos_by_name
+
+log = logging.getLogger(__name__)
 
 
 def start_locator_thread(args):
-    search_thread = Thread(target=search, args=(args,))
+    search_thread = Thread(target=search_loop, args=(args,))
     search_thread.daemon = True
     search_thread.name = 'search_thread'
     search_thread.start()
@@ -27,12 +30,16 @@ if __name__ == '__main__':
     logging.getLogger("pogom.pgoapi.pgoapi").setLevel(logging.WARNING)
     logging.getLogger("pogom.pgoapi.rpc_api").setLevel(logging.INFO)
 
-    # logging.getLogger("requests").setLevel(logging.DEBUG)
-    # logging.getLogger("pgoapi").setLevel(logging.DEBUG)
-    # logging.getLogger("rpc_api").setLevel(logging.DEBUG)
-
     args = get_args()
     create_tables()
+    
+    position = get_pos_by_name(args.location)
+    log.info('Parsed location is: {:.4f}/{:.4f}/{:.4f} (lat/lng/alt)'.
+             format(*position))
+
+    config['ORIGINAL_LATITUDE'] = position[0]
+    config['ORIGINAL_LONGITUDE'] = position[1]
+
 
     if not args.mock:
         start_locator_thread(args)
