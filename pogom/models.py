a25f10b9499772959f00f12e0775d5c1fac6c4f3
nbo <n.borrmann@googlemail.com>
2016-07-21 16:25:19 +0200

    changed bulk sql size to 100 for performance

--- pogom/models.py
+++ pogom/models.py
@@ -147,7 +147,7 @@ def parse_map(map_dict):
 def bulk_upsert(cls, data):
     num_rows = len(data.values())
     i = 0
-    step = 50
+    step = 100
     
     while i < num_rows:
         log.debug("Inserting items {} to {}".format(i, min(i+step, num_rows)))
