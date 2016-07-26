159d6c6152d718a986d183fc185c7dc7ba73e714
nbo <nborrmann@users.noreply.github.com>
2016-07-26 17:13:37 +0200

    Update __init__.py

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,5 +5,5 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'static/locales',
     'ROOT_PATH': None,
-    'GOOGLEMAPS_KEY': 'AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4'
+    'GOOGLEMAPS_KEY': 'ENTER_KEY_HERE'
 }
